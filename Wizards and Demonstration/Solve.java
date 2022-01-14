548e3ce740769284297e932d52270ac9ab23ac72
HIMANSHU RANJAN <45516142+himanshu-red@users.noreply.github.com>
2022-01-14 12:18:25 +0530

    Create Solve.java

---
+++ Wizards and Demonstration/Solve.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.Collections;
+import java.lang.Math;
+public class Solve
+{
+   public static void main(String[] args)
+   {
+       Scanner input = new Scanner (System.in);
+       int n = input.nextInt(); // total population
+       int x = input.nextInt(); // no of wizards
+       int y = input.nextInt(); // percentage needed
+       double perc = Math.ceil(n * (y / (double)100));
+//       System.out.println((Math.ceil(n * (y / (double)100))));
+       if (perc > x)
+           System.out.println((int)(Math.ceil(perc - x))) ;
+       else
+           System.out.println("0");
+    }
+}
