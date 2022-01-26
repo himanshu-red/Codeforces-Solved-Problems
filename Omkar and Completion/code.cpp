11bdc37af4959386d2d0e022296f87da43e604fd
HIMANSHU RANJAN <45516142+himanshu-red@users.noreply.github.com>
2022-01-26 09:19:09 +0530

    Create code.cpp

---
+++ Omkar and Completion/code.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std; 
+int main()
+{
+    int t, num; 
+    cin >> t; 
+    while (t--)
+    {
+        cin >> num; 
+        for (int i = 0; i< num; i++)
+        {
+            cout << 1 << " " ; 
+        }
+        cout << endl; 
+    }
+}
