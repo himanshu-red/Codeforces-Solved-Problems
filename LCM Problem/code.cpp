296b3acee6c9b9be2c0940861167f18a8d352bc4
HIMANSHU RANJAN <45516142+himanshu-red@users.noreply.github.com>
2022-01-26 10:38:27 +0530

    Create code.cpp

---
+++ LCM Problem/code.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int a, b;
+        cin >> a >> b;
+        if (a * 2 <= b)
+        {
+            cout << a << " " << a * 2 << endl;
+        }
+        else
+            cout << -1 << " " << -1 << endl;
+    }
+}
