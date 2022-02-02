5193ff230e28359f4a58612884de0f71922392a8
HIMANSHU RANJAN <45516142+himanshu-red@users.noreply.github.com>
2022-02-02 10:39:04 +0530

    Create code.cpp

---
+++ Ehab and Another Construction Problem/code.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = unsigned long long;
+int main()
+{
+    int t;
+    cin >> t;
+    if (t == 1)
+        cout << -1;
+    else
+    {
+        cout << t << " " << t << endl;
+    }
+}
