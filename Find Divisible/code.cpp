aa6360d5f850b7eecc7737914a6d46ccfe0f6f82
HIMANSHU RANJAN <45516142+himanshu-red@users.noreply.github.com>
2022-02-03 11:45:01 +0530

    Create code.cpp

---
+++ Find Divisible/code.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std; 
+int main()
+{
+    int t, l, r; 
+    cin >> t; 
+    while (t--)
+    {
+        cin >> l >> r; 
+        cout << l << " " << l*2 << endl; 
+    }
+}
